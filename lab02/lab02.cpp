45f7b4885d8b9bb5088ba3ff35fe856356a87f10
Norbert Lesniak <s20151@pjwstk.edu.pl>
2021-10-11 12:00:08 +0200

    end of classes

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -1,31 +1,27 @@
-//
-// Created by s20151 on 11.10.2021.
-//
 #include <fstream>
 #include <string>
 #include <vector>
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
-int main() {
+//Przykład: {1 2 3 4 5 6} -> {1, 5, 9}, {2, 6, 7} suma = 15
+
+vector<int> wczytaj_dane(string sciezka){
     vector<int> dane;
-    string sciezka = "Z:\\MHE\\lab02\\liczby.txt";
     ifstream plik;
     plik.open(sciezka);
-    if(!plik){
-        cout<<"Bład. Nie udało się otworzyć pliku.";
-    }
+    if(!plik) cout<<"Bład. Nie udało się otworzyć pliku.";
     int liczba;
-    while(plik >> liczba){
-        dane.push_back(liczba);
-    }
+    while(plik >> liczba) dane.push_back(liczba);
     plik.close();
+    sort(dane.begin(), dane.end());
+    return dane;
+}
+
+int main() {
+    vector<int> dane = wczytaj_dane("Z:\\MHE\\lab02\\liczby.txt");
     cout<< "Given numbers:" <<endl;
     cout<< "{ "; for(int x : dane) cout<< x<<" "; cout<<"}"<<endl;
-//    for(int i=0;i<dane.size();i++){
-//        cout<<dane[i]<<" ";
-//        if((i+1)%3==0) cout<<endl;
-//    }
     return 0;
 }
