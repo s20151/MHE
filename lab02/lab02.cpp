21a434f184ddc12484e74e06bcf41528ea0561de
Norbert Lesniak <s20151@pjwstk.edu.pl>
2021-10-11 11:28:21 +0200

    implemented loading numbers from the file and printing them in terminal

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -1,4 +1,31 @@
 //
 // Created by s20151 on 11.10.2021.
 //
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    vector<int> dane;
+    string sciezka = "Z:\\MHE\\lab02\\liczby.txt";
+    ifstream plik;
+    plik.open(sciezka);
+    if(!plik){
+        cout<<"Bład. Nie udało się otworzyć pliku.";
+    }
+    int liczba;
+    while(plik >> liczba){
+        dane.push_back(liczba);
+    }
+    plik.close();
+    cout<< "Given numbers:" <<endl;
+    cout<< "{ "; for(int x : dane) cout<< x<<" "; cout<<"}"<<endl;
+//    for(int i=0;i<dane.size();i++){
+//        cout<<dane[i]<<" ";
+//        if((i+1)%3==0) cout<<endl;
+//    }
+    return 0;
+}
