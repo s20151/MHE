4a8aa4347c79bafa5a1062e8c976b9774fb01072
s20151 <s20151@pjwstk.edu.pl>
2021-10-17 22:28:18 +0200

    moved printing to separate function

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -14,26 +14,35 @@ vector<int> load(string path){
     file.open(path);
     if(!file) {
         cout<<"Error opening selected file.";
+        file.close();
     }else {
         int number;
         while (file >> number) data.push_back(number);
         file.close();
         sort(data.begin(), data.end());
-        return data;
     }
+    return data;
 }
 
-int main(int argc, char** argv) {
-    string dir = "C:\\Users\\Admin\\Desktop\\SEM5\\MHE\\lab02\\"; // Z:\MHE\lab02
-    vector<int> data = load(dir + argv[1]);
-    if(data.empty()){
-        return 0;
-    }else{
+void print(vector<int> data){
+    if(data.size()>0){
         cout << "Given numbers:" << endl;
         cout << "{ ";
         for (int x: data) cout << x << " ";
         cout << "}" << endl;
+    }else {
+        cout<< "Could not find any data to print.";
+    }
+}
+
+
+int main(int argc, char** argv) {
+    string dir = "C:\\Users\\Admin\\Desktop\\SEM5\\MHE\\lab02\\"; // Z:\MHE\lab02
+    if(argc == 2) {
+        vector<int> data = load(dir + argv[1]);
+        print(data);
+    }else {
+        cout << "You did not enter file name."<<endl;
     }
     return 0;
-
 }
