be2e8a63156bdc85a70a9d5ac3813fa9c02d8ca0
s20151 <s20151@pjwstk.edu.pl>
2021-10-17 20:24:58 +0200

    Polish -> English translation, implemented loading file name from terminal

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -3,25 +3,37 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
 //Przykład: {1 2 3 4 5 6} -> {1, 5, 9}, {2, 6, 7} suma = 15
 
-vector<int> wczytaj_dane(string sciezka){
-    vector<int> dane;
-    ifstream plik;
-    plik.open(sciezka);
-    if(!plik) cout<<"Bład. Nie udało się otworzyć pliku.";
-    int liczba;
-    while(plik >> liczba) dane.push_back(liczba);
-    plik.close();
-    sort(dane.begin(), dane.end());
-    return dane;
+vector<int> load(string path){
+    vector<int> data;
+    ifstream file;
+    file.open(path);
+    if(!file) {
+        cout<<"Error opening selected file.";
+    }else {
+        int number;
+        while (file >> number) data.push_back(number);
+        file.close();
+        sort(data.begin(), data.end());
+        return data;
+    }
 }
 
-int main() {
-    vector<int> dane = wczytaj_dane("Z:\\MHE\\lab02\\liczby.txt");
-    cout<< "Given numbers:" <<endl;
-    cout<< "{ "; for(int x : dane) cout<< x<<" "; cout<<"}"<<endl;
+int main(int argc, char** argv) {
+    string dir = "C:\\Users\\Admin\\Desktop\\SEM5\\MHE\\lab02\\"; // Z:\MHE\lab02
+    vector<int> data = load(dir + argv[1]);
+    if(data.empty()){
+        return 0;
+    }else{
+        cout << "Given numbers:" << endl;
+        cout << "{ ";
+        for (int x: data) cout << x << " ";
+        cout << "}" << endl;
+    }
     return 0;
+
 }
