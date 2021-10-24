417ab557d06301ee468fc746c837c2002f05bda3
s20151 <s20151@pjwstk.edu.pl>
2021-10-24 19:13:20 +0200

    Added generating random triplets

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -21,7 +21,6 @@ vector<int> load(string path){
 }
 
 void print(vector<int> data){
-        cout << "Given numbers:" << endl;
         cout << "{ ";
         for (int x: data) cout << x << " ";
         cout << "}" << endl;
@@ -37,13 +36,30 @@ bool isValid(vector<int> data){
     }
 }
 
-double goal_function(vector<int> solution){}
+//double goal_function(vector<int> solution){}
 
-vector<int> generate_solution(vector<int> input){
-
+vector<vector<int>> generate_random_working_point(vector<int> input){
+    vector<int> numbers = input;
+    int v_size = numbers.size();
+    vector<vector<int>> triplets;
+    for(int i=0; i<v_size/3; i++){
+        vector<int> triplet;
+        for(int j=0; j<3; j++){
+            srand( time( NULL ) );
+            int random_number = rand()%numbers.size();
+            triplet.push_back(numbers[random_number]);
+            numbers.erase(numbers.begin() + random_number);
+        }
+        triplets.push_back(triplet);
+    }
+    return triplets;
 }
 
-vector<int> next_solution(vector<int> solution){}
+void print_triplets(vector<vector<int>> triplets){
+    for (vector<int> x: triplets) print(x);
+}
+
+//vector<int> next_solution(vector<int> solution){}
 
 
 
@@ -54,7 +70,11 @@ int main(int argc, char** argv) {
     if(argc > 1) {
         vector<int> data = load( argv[1] );
         if(isValid(data)) {
+            cout << "Given numbers:" << endl;
             print(data);
+            vector<vector<int>> random = generate_random_working_point(data);
+            cout << "Random triplets:" << endl;
+            print_triplets(random);
         }else {
             cout << "Loaded data is not valid. " << endl;
         }
